ceb929f47de7542f226ab4ff538aff0dc884ce2d
James McKay <jamestm97@gmail.com>
2017-11-01 16:33:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ loop (let i = 0; i < 25; i++) {
   if (i === 1) {
     array.push "I am 1 strange loop.";
   } else {
-    return `I am ${i} stranges loops`;
+    array.push `I am ${i} stranges loops`;
   }
 }
 }
